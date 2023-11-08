e82643990fbb6787727a431ebeb3690682bb1ad2
unknown <rezwanislam857@gmail.com>
2023-11-08 14:47:48 -0800

    Added NewClassFTGitHub.java in com.generic

---
+++ src/main/java/com/generic/NewClassFTGitHub.java
@@ -0,0 +1,11 @@
+package com.generic;
+
+public class NewClassFTGitHub {
+	public void getNewMetod() {
+		System.out.println("This is a new class for testing GitHub");
+	}
+public static void main(String[] args) {
+	NewClassFTGitHub obj=new NewClassFTGitHub();
+	obj.getNewMetod();
+}
+}
