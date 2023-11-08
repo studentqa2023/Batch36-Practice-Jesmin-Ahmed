9b876b1e0430c5d8a9bd570845cb6e75eb28f38b
unknown <rezwanislam857@gmail.com>
2023-11-08 13:46:25 -0800

    added JesminClass in com.generic

---
+++ src/main/java/com/generic/JesminClass.java
@@ -0,0 +1,12 @@
+package com.generic;
+
+public class JesminClass {
+	
+	public static void newMethod() {
+		System.out.println("this is a test");
+	}
+	public static void main(String[] args) {
+		newMethod();
+	}
+
+}
